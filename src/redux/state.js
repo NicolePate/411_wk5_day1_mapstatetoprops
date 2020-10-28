3710815a72a87aa8d6fd8939ba903785e826d12a
NicolePate <nicole.pate512@gmail.com>
2020-10-28 12:50:01 -0500

    done

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,11 @@
 export default {
     // Add user property here
+    user: [
+      {
+         username: 'user',
+         email: 'user@test.com'
+     }
+    ],
     cars: [
         {
            "id": 1,
